ef78f56998ab01877c44c60ef7caad10d9a5c123
PuZZleDucK <puzzleduck@gmail.com>
2017-08-18 18:36:22 +1000

    fix incorrect assumption and test

--- test.sh
+++ test.sh
@@ -6,6 +6,9 @@ test_files=$(ls data/)
 for file in data/*; do
   if [[ $file == "data/"*"test"* ]]; then # limit to tests
     expected_output=$(tail -1 $file)
+    if [[ $expected_output == "Expected output:" ]]; then # expect blank output
+      expected_output=""
+    fi
     output=$($program_name $file)
     if [[ "$output" == "$expected_output" ]]; then
       echo -e "$file - \e[32mpass\e[0m"
