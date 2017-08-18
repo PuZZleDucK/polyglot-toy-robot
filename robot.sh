b863f462955d83c3c77b6d2b6d1d7ca2f62017fc
PuZZleDucK <puzzleduck@gmail.com>
2017-08-18 14:06:32 +1000

    parse commands

--- robot.sh
+++ robot.sh
@@ -1,3 +1,13 @@
 #!/bin/bash
+if [ ! -f "$1" ]; then
+  echo "usage: robot.sh <toy-script>"
+  exit
+fi
 
 echo "Hi Robot"
+input=$(tr ' ' '-' < $1)
+for input_line in $input ; do
+  command=$(echo $input_line | sed -E 's/(.*)-(.*)/\1/')
+  echo "  command: $command"
+
+done
