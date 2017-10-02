cb9681c00796bbb77401ad7444cec53d5676ac3c
PuZZleDucK <puzzleduck@gmail.com>
2017-10-02 20:28:18 +1100

    update test framework to refer to new data location

--- test/integration/test-bash.sh
+++ test/integration/test-bash.sh
@@ -1,22 +1,20 @@
 #!/bin/bash
 set -o errexit
 
-export program_name="./robot.sh"
-test_files=$(ls data/)
-for file in data/*; do
-  if [[ $file == "data/"* ]]; then
-    output=$($program_name $file)
-    expected_output=$(tail -1 $file)
-    if [[ $expected_output == "Expected output:" ]]; then
-      # There was no output to find, expect blank output
-      expected_output=""
-    fi
-    if [[ "$output" == "$expected_output" ]]; then
-      echo -e "$file - \e[32mpass\e[0m"
-    else
-      echo -e "$file - \e[31mFAIL\e[0m"
-      echo -e "    expected_output: $expected_output"
-      echo -e "    output: $output"
-    fi
+program_name="./lib/bash/robot.sh"
+test_folder="test/fixtures/files/"
+test_files=$(ls $test_folder)
+for file in $test_files; do
+  output=$($program_name $test_folder$file)
+  expected_output=$(tail -1 $test_folder$file)
+  if [[ $expected_output == "Expected output:" ]]; then
+    expected_output="" # No output to find, expect blank output
+  fi
+  if [[ "$output" == "$expected_output" ]]; then
+    echo -e "$file - \e[32mpass\e[0m"
+  else
+    echo -e "$file - \e[31mFAIL\e[0m"
+    echo -e "    expected_output: $expected_output"
+    echo -e "    output: $output"
   fi
 done
